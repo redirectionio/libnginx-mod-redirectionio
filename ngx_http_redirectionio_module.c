5dce4e331fde3cecca8358bd41eb734fddfb7825
Joel Wurtz <jwurtz@jolicode.com>
2018-09-19 16:48:03 +0200

    WIP Upstream

--- ngx_http_redirectionio_module.c
+++ ngx_http_redirectionio_module.c
@@ -14,17 +14,16 @@ static const char LOG_QUERY[] = "Log\n{ \"project_key\": \"%V\", \"source_url\":
 
 #define ngx_str_to_go_str(ngx) (GoString){ (const char*)ngx.data, ngx.len }
 
-#if !defined(NGX_HTTP_PRECONTENT_PHASE)
-#define NGX_HTTP_PRECONTENT_PHASE NGX_HTTP_PREACCESS_PHASE
-#endif
-
 typedef struct {
-    ngx_uint_t  enable;
-    ngx_uint_t  enable_logs;
-    ngx_str_t   project_key;
+    ngx_uint_t                  enable;
+    ngx_uint_t                  enable_logs;
+    ngx_str_t                   project_key;
+    ngx_http_upstream_conf_t    upstream;
+    ngx_http_complex_value_t    *complex_target;
 } ngx_http_redirectionio_conf_t;
 
 typedef struct {
+    ngx_uint_t  enable;
     ngx_str_t   instance_name;
     ngx_str_t   api_host;
     ngx_str_t   user_agent;
@@ -37,11 +36,12 @@ typedef struct {
 typedef void (*ngx_http_redirectionio_read_handler_t)(ngx_event_t *rev, ngx_str_t *line);
 
 typedef struct {
-    ngx_peer_connection_t                                                                           peer;
-    ngx_str_t                                                                                       matched_rule_id;
-    ngx_str_t                                                                                       target;
-    ngx_uint_t                                                                                      status;
-    ngx_http_redirectionio_read_handler_t                                                           read_handler;
+    ngx_peer_connection_t                   peer;
+    ngx_str_t                               matched_rule_id;
+    ngx_str_t                               target;
+    ngx_uint_t                              status;
+    ngx_http_redirectionio_read_handler_t   read_handler;
+    ngx_http_request_t                      *subrequest;
 } ngx_http_redirectionio_ctx_t;
 
 /**
@@ -57,6 +57,7 @@ static void *ngx_http_redirectionio_create_agent_conf(ngx_conf_t *cf);
 static char *ngx_http_redirectionio_init_agent_conf(ngx_conf_t *cf, void *child);
 static void *ngx_http_redirectionio_create_conf(ngx_conf_t *cf);
 static char *ngx_http_redirectionio_merge_conf(ngx_conf_t *cf, void *parent, void *child);
+static char *ngx_http_redirectionio_upstream(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 static ngx_int_t ngx_http_redirectionio_init_process(ngx_cycle_t *cycle);
 static ngx_int_t ngx_http_redirectionio_postconfiguration(ngx_conf_t *cf);
@@ -109,10 +110,26 @@ static ngx_command_t ngx_http_redirectionio_commands[] = {
         ngx_http_redirectionio_enable_state
     },
     {
+        ngx_string("redirectionio_upstream"),
+        NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_SIF_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF|NGX_CONF_TAKE1,
+        ngx_http_redirectionio_upstream,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        0,
+        NULL
+    },
+    {
+        ngx_string("redirectionio_agent_enable"),
+        NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
+        ngx_conf_set_enum_slot,
+        NGX_HTTP_MAIN_CONF_OFFSET,
+        offsetof(ngx_http_redirectionio_agent_conf_t, enable),
+        ngx_http_redirectionio_enable_state
+    },
+    {
         ngx_string("redirectionio_host"),
         NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
         ngx_conf_set_str_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
+        NGX_HTTP_MAIN_CONF_OFFSET,
         offsetof(ngx_http_redirectionio_agent_conf_t, api_host),
         NULL
     },
@@ -120,7 +137,7 @@ static ngx_command_t ngx_http_redirectionio_commands[] = {
         ngx_string("redirectionio_instance_name"),
         NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
         ngx_conf_set_str_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
+        NGX_HTTP_MAIN_CONF_OFFSET,
         offsetof(ngx_http_redirectionio_agent_conf_t, instance_name),
         NULL
     },
@@ -128,7 +145,7 @@ static ngx_command_t ngx_http_redirectionio_commands[] = {
         ngx_string("redirectionio_datadir"),
         NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
         ngx_conf_set_str_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
+        NGX_HTTP_MAIN_CONF_OFFSET,
         offsetof(ngx_http_redirectionio_agent_conf_t, data_directory),
         NULL
     },
@@ -136,7 +153,7 @@ static ngx_command_t ngx_http_redirectionio_commands[] = {
         ngx_string("redirectionio_debug"),
         NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
         ngx_conf_set_enum_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
+        NGX_HTTP_MAIN_CONF_OFFSET,
         offsetof(ngx_http_redirectionio_agent_conf_t, debug),
         ngx_http_redirectionio_enable_state
     },
@@ -144,21 +161,20 @@ static ngx_command_t ngx_http_redirectionio_commands[] = {
         ngx_string("redirectionio_persist"),
         NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
         ngx_conf_set_enum_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
+        NGX_HTTP_MAIN_CONF_OFFSET,
         offsetof(ngx_http_redirectionio_agent_conf_t, persist),
         ngx_http_redirectionio_enable_state
     },
     {
         ngx_string("redirectionio_cache"),
-        NGX_HTTP_MAIN_CONF|NGX_DIRECT_CONF|NGX_CONF_TAKE1,
+        NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
         ngx_conf_set_enum_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
+        NGX_HTTP_MAIN_CONF_OFFSET,
         offsetof(ngx_http_redirectionio_agent_conf_t, cache),
         ngx_http_redirectionio_enable_state
     },
     ngx_null_command /* command termination */
 };
-
 
 /* The module context. */
 static ngx_http_module_t ngx_http_redirectionio_module_ctx = {
@@ -196,6 +212,55 @@ static ngx_int_t ngx_http_redirectionio_init_process(ngx_cycle_t *cycle) {
     return NGX_OK;
 }
 
+static char *ngx_http_redirectionio_upstream(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
+    ngx_http_redirectionio_conf_t       *rcf = conf;
+    ngx_url_t                           url;
+    ngx_http_compile_complex_value_t    ccv;
+    ngx_str_t                           *value;
+    ngx_uint_t                          n;
+
+    if (rcf->upstream.upstream) {
+        return "is duplicate";
+    }
+
+    value = cf->args->elts;
+    n = ngx_http_script_variables_count(&value[1]);
+
+    if (n) {
+        rcf->complex_target = ngx_palloc(cf->pool, sizeof(ngx_http_complex_value_t));
+
+        if (rcf->complex_target == NULL) {
+            return NGX_CONF_ERROR;
+        }
+
+        ngx_memzero(&ccv, sizeof(ngx_http_compile_complex_value_t));
+        ccv.cf = cf;
+        ccv.value = &value[1];
+        ccv.complex_value = rcf->complex_target;
+
+        if (ngx_http_compile_complex_value(&ccv) != NGX_OK) {
+            return NGX_CONF_ERROR;
+        }
+
+        return NGX_CONF_OK;
+    }
+
+    rcf->complex_target = NULL;
+
+    ngx_memzero(&url, sizeof(ngx_url_t));
+
+    url.url = value[1];
+    url.no_resolve = 1;
+
+    rcf->upstream.upstream = ngx_http_upstream_add(cf, &url, 0);
+
+    if (rcf->upstream.upstream == NULL) {
+        return NGX_CONF_ERROR;
+    }
+
+    return NGX_CONF_OK;
+}
+
 /**
  * Init
  *
@@ -223,28 +288,32 @@ static ngx_int_t ngx_http_redirectionio_postconfiguration(ngx_conf_t *cf) {
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
-    create_ctx_handler = ngx_array_push(&cmcf->phases[NGX_HTTP_PREACCESS_PHASE].handlers);
-    redirect_handler = ngx_array_push(&cmcf->phases[NGX_HTTP_PRECONTENT_PHASE].handlers);
     log_handler = ngx_array_push(&cmcf->phases[NGX_HTTP_LOG_PHASE].handlers);
-
-    if (create_ctx_handler == NULL) {
-        ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->cycle->log, 0, "redirectionio: init(): error pushing ctx handler");
-        return NGX_ERROR;
-    }
-
-    if (redirect_handler == NULL) {
-        ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->cycle->log, 0, "redirectionio: init(): error pushing redirect handler");
-        return NGX_ERROR;
-    }
 
     if (log_handler == NULL) {
         ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->cycle->log, 0, "redirectionio: init(): error pushing log handler");
         return NGX_ERROR;
     }
 
-    *create_ctx_handler = ngx_http_redirectionio_create_ctx_handler;
-    *redirect_handler = ngx_http_redirectionio_redirect_handler;
     *log_handler = ngx_http_redirectionio_log_handler;
+
+    redirect_handler = ngx_array_push(&cmcf->phases[NGX_HTTP_PREACCESS_PHASE].handlers);
+
+    if (redirect_handler == NULL) {
+        ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->cycle->log, 0, "redirectionio: init(): error pushing redirect handler");
+        return NGX_ERROR;
+    }
+
+    *redirect_handler = ngx_http_redirectionio_redirect_handler;
+
+    create_ctx_handler = ngx_array_push(&cmcf->phases[NGX_HTTP_PREACCESS_PHASE].handlers);
+
+    if (create_ctx_handler == NULL) {
+        ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->cycle->log, 0, "redirectionio: init(): error pushing ctx handler");
+        return NGX_ERROR;
+    }
+
+    *create_ctx_handler = ngx_http_redirectionio_create_ctx_handler;
 
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->cycle->log, 0, "redirectionio: init(): return OK");
 
@@ -400,9 +469,10 @@ static void *ngx_http_redirectionio_create_agent_conf(ngx_conf_t *cf) {
         return NGX_CONF_ERROR;
     }
 
-    conf->debug = NGX_CONF_UNSET;
-    conf->persist = NGX_CONF_UNSET;
-    conf->cache = NGX_CONF_UNSET;
+    conf->debug = NGX_CONF_UNSET_UINT;
+    conf->persist = NGX_CONF_UNSET_UINT;
+    conf->cache = NGX_CONF_UNSET_UINT;
+    conf->enable = NGX_CONF_UNSET_UINT;
 
     return conf;
 }
@@ -410,19 +480,18 @@ static void *ngx_http_redirectionio_create_agent_conf(ngx_conf_t *cf) {
 static char *ngx_http_redirectionio_init_agent_conf(ngx_conf_t *cf, void *child) {
     ngx_http_redirectionio_agent_conf_t *conf = child;
 
-    ngx_conf_init_uint_value(conf->debug, NGX_HTTP_REDIRECTIONIO_ON);
-    ngx_conf_init_uint_value(conf->persist, NGX_HTTP_REDIRECTIONIO_OFF);
-    ngx_conf_init_uint_value(conf->cache, NGX_HTTP_REDIRECTIONIO_OFF);
+    ngx_conf_init_uint_value(conf->debug, NGX_HTTP_REDIRECTIONIO_OFF);
+    ngx_conf_init_uint_value(conf->persist, NGX_HTTP_REDIRECTIONIO_ON);
+    ngx_conf_init_uint_value(conf->cache, NGX_HTTP_REDIRECTIONIO_ON);
+    ngx_conf_init_uint_value(conf->enable, NGX_HTTP_REDIRECTIONIO_ON);
 
     if (conf->instance_name.data == NULL) {
         conf->instance_name.len = cf->cycle->hostname.len;
         conf->instance_name.data = cf->cycle->hostname.data;
-
-        conf->instance_name = (ngx_str_t)ngx_string("Nginx instance");
     }
 
     if (conf->api_host.data == NULL) {
-        conf->api_host = (ngx_str_t)ngx_string("https://api.redirection-io.test/app_dev.php");
+        conf->api_host = (ngx_str_t)ngx_string("https://api.redirection.io");
     }
 
     if (conf->data_directory.data == NULL) {
@@ -438,7 +507,7 @@ static char *ngx_http_redirectionio_init_agent_conf(ngx_conf_t *cf, void *child)
 
 /* Create configuration object */
 static void *ngx_http_redirectionio_create_conf(ngx_conf_t *cf) {
-    ngx_http_redirectionio_conf_t *conf;
+    ngx_http_redirectionio_conf_t   *conf;
 
     conf = (ngx_http_redirectionio_conf_t *) ngx_pcalloc(cf->pool, sizeof(ngx_http_redirectionio_conf_t));
 
@@ -446,15 +515,67 @@ static void *ngx_http_redirectionio_create_conf(ngx_conf_t *cf) {
         return NGX_CONF_ERROR;
     }
 
-    conf->enable = NGX_CONF_UNSET;
-    conf->enable_logs = NGX_CONF_UNSET;
+    conf->enable = NGX_CONF_UNSET_UINT;
+    conf->enable_logs = NGX_CONF_UNSET_UINT;
+    conf->upstream.connect_timeout = NGX_CONF_UNSET_MSEC;
+    conf->upstream.send_timeout = NGX_CONF_UNSET_MSEC;
+    conf->upstream.read_timeout = NGX_CONF_UNSET_MSEC;
+
+    conf->upstream.buffer_size = NGX_CONF_UNSET_SIZE;
+
+    /* the hardcoded values */
+    conf->upstream.cyclic_temp_file = 0;
+    conf->upstream.buffering = 0;
+    conf->upstream.ignore_client_abort = 1;
+    conf->upstream.send_lowat = 0;
+    conf->upstream.bufs.num = 0;
+    conf->upstream.busy_buffers_size = 0;
+    conf->upstream.max_temp_file_size = 0;
+    conf->upstream.temp_file_write_size = 0;
+    conf->upstream.intercept_errors = 1;
+    conf->upstream.intercept_404 = 1;
+    conf->upstream.pass_request_headers = 0;
+    conf->upstream.pass_request_body = 0;
 
     return conf;
 }
 
 static char *ngx_http_redirectionio_merge_conf(ngx_conf_t *cf, void *parent, void *child) {
-    ngx_http_redirectionio_conf_t *prev = parent;
-    ngx_http_redirectionio_conf_t *conf = child;
+    ngx_http_redirectionio_conf_t   *prev = parent;
+    ngx_http_redirectionio_conf_t   *conf = child;
+    ngx_url_t                       url;
+
+    ngx_conf_merge_msec_value(conf->upstream.connect_timeout, prev->upstream.connect_timeout, 1000);
+    ngx_conf_merge_msec_value(conf->upstream.send_timeout, prev->upstream.send_timeout, 1000);
+    ngx_conf_merge_msec_value(conf->upstream.read_timeout, prev->upstream.read_timeout, 1000);
+    ngx_conf_merge_size_value(conf->upstream.buffer_size, prev->upstream.buffer_size, (size_t) ngx_pagesize);
+
+    ngx_conf_merge_bitmask_value(conf->upstream.next_upstream, prev->upstream.next_upstream, (
+        NGX_CONF_BITMASK_SET
+        | NGX_HTTP_UPSTREAM_FT_ERROR
+        | NGX_HTTP_UPSTREAM_FT_TIMEOUT
+    ));
+
+    if (conf->upstream.next_upstream & NGX_HTTP_UPSTREAM_FT_OFF) {
+        conf->upstream.next_upstream = NGX_CONF_BITMASK_SET | NGX_HTTP_UPSTREAM_FT_OFF;
+    }
+
+    if (conf->upstream.upstream == NULL) {
+        if (prev->upstream.upstream == NULL) {
+            ngx_memzero(&url, sizeof(ngx_url_t));
+
+            url.url = (ngx_str_t)ngx_string("unix://tmp/agent.sock");
+            url.no_resolve = 1;
+
+            conf->upstream.upstream = ngx_http_upstream_add(cf, &url, 0);
+
+            if (conf->upstream.upstream == NULL) {
+                return NGX_CONF_ERROR;
+            }
+        } else {
+            conf->upstream.upstream = prev->upstream.upstream;
+        }
+    }
 
     ngx_conf_merge_uint_value(conf->enable, prev->enable, NGX_HTTP_REDIRECTIONIO_OFF);
     ngx_conf_merge_uint_value(conf->enable_logs, prev->enable_logs, NGX_HTTP_REDIRECTIONIO_ON);
@@ -660,8 +781,7 @@ static void ngx_http_redirectionio_read_get_status_handler(ngx_event_t *rev, ngx
     r = c->data;
 
     response = (u_char *) ngx_pcalloc(r->pool, line->len + 1);
-    response = ngx_copy(response, line->data, line->len);
-    *(response + line->len + 1) = '\0';
+    *(ngx_copy(response, line->data, line->len) + 1) = '\0';
 
     ctx = ngx_http_get_module_ctx(r, ngx_http_redirectionio_module);
     ctx->status = atoi((const char*)response);
