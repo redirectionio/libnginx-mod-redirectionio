45fa8f8cbdfd80a3f0b9da1602f47b9a00c34146
Joel Wurtz <jwurtz@jolicode.com>
2018-11-19 18:05:26 +0100

    Try to use better handler for redirection io one

--- src/ngx_http_redirectionio_module.c
+++ src/ngx_http_redirectionio_module.c
@@ -140,7 +140,11 @@ static ngx_int_t ngx_http_redirectionio_postconfiguration(ngx_conf_t *cf) {
 
     *log_handler = ngx_http_redirectionio_log_handler;
 
-    redirect_handler = ngx_array_push(&cmcf->phases[NGX_HTTP_PREACCESS_PHASE].handlers);
+#ifdef NGX_HTTP_PRECONTENT_PHASE
+    redirect_handler = ngx_array_push(&cmcf->phases[NGX_HTTP_PRECONTENT_PHASE].handlers);
+#else
+    redirect_handler = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
+#endif
 
     if (redirect_handler == NULL) {
         ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->cycle->log, 0, "redirectionio: init(): error pushing redirect handler");
